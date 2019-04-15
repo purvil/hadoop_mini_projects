414b7cb1fb437bb6ed70672d629630e9f7891dee
purvil <davepurvil@gmail.com>
2019-04-15 21:38:21 +0000

    Find total number of request per resource file.

---
+++ total_hits_per_file/reducer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+
+def reducer():
+    oldKey = None
+    totalRequest = 0
+    for line in sys.stdin:
+	data = line.strip().split('\t')
+	if len(data) != 2:
+	    continue
+	thisKey, count = data
+	if oldKey and oldKey != thisKey:
+	    print "{0}\t{1}".format(oldKey, totalRequest)
+	    totalRequest = 0
+	oldKey = thisKey
+	totalRequest += float(count)
+
+    if oldKey:
+	print "{0}\t{1}".format(oldKey, totalRequest)
+
+
+
+if __name__ == "__main__":
+    reducer()
