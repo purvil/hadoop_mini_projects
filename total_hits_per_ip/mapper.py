06fba618f307ca5746e3238092e5979031e2953f
purvil <davepurvil@gmail.com>
2019-04-15 21:45:08 +0000

    Determine total number of request from each ip

---
+++ total_hits_per_ip/mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+def mapper():
+    for line in sys.stdin:
+	data = line.strip().split(' ')
+	if len(data) != 10:
+	    continue
+	ip, client_id, client_uname, date, tz, method, resource, protocol, status_code, size = data  
+	date = date[1:]
+	method = method[1:]
+	protocol = protocol[:-1]
+	tz = tz[:-1]
+	print "{0}\t{1}".format(ip, 1)
+
+
+if __name__ == "__main__":
+    mapper()
